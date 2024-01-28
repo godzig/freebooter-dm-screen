d0606a560555effd419cc074e4f5810aee608741
Mike Godwin <mike.godwin@graphitehealth.io>
2024-01-28 12:35:47 -0800

    mapKeys added to SpellName component

--- src/components/SpellName.js
+++ src/components/SpellName.js
@@ -1,5 +1,5 @@
 // Generates spell names per Freebooter tables
-import { pick, wizardName } from "../app/utils";
+import { pick, wizardName, roll, mapKeys } from "../app/utils";
 import { useState } from 'react'
 
 const form = ["Armor", "Arrow", "Aura", "Bane", "Beast", "Blade", "Blast", "Blessing", "Blob", "Blood", "Bolt", "Bond", "Boon", "Brain", "Burst", "Call", "Charm", "Circle", "Claw", "Cloak", "Cone", "Crown", "Cube", "Cup", "Curse", "Dagger", "Dart", "Demon", "Disturbance", "Door", "Eye", "Eyes", "Face", "Fang", "Feast", "Finger", "Fissure", "Fist", "Gate", "Gaze", "Glamour", "Globe", "Golem", "Guard", "Guide", "Guise", "Halo", "Hammer", "Hand", "Heart", "Helm", "Horn", "Jar", "Lock", "Mantle", "Mark", "Memory", "Mind", "Mouth", "Oath", "Oracle", "Pattern", "Pet", "Pillar", "Pocket", "Portal", "Pyramid", "Ray", "Rune", "Scream", "Seal", "Sentinel", "Servant", "Shaft", "Shield", "Sigil", "Sign", "Song", "Spear", "Sphere", "Spray", "Staff", "Storm", "Strike", "Sword", "Tendril", "Tongue", "Tooth", "Trap", "Veil", "Voice", "Wall", "Ward", "Wave", "Weapon", "Weave", "Whisper", "Wings", "Word"];
@@ -7,29 +7,19 @@ const property = ["Acid", "Aether", "Air", "Anger", "Ash", "Avarice", "Balance",
 const adjective = ["All-Knowing", "All-Seeing", "Arcane", "Befuddling", "Binding", "Black", "Blazing", "Blinding", "Bloody", "Bright", "Cacophonous", "Cerulean", "Concealing", "Confusing", "Consuming", "Crimson", "Damnable", "Dark", "Deflecting", "Delicate", "Demonic", "Devastating", "Devilish", "Diminishing", "Draining", "Eldritch", "Empowering", "Enlightening", "Ensorcelling", "Entangling", "Enveloping", "Erratic", "Evil", "Excruciating", "Expanding", "Extra-Planar", "Fearsome", "Flaming", "Floating", "Freezing", "Glittering", "Gyrating", "Helpful", "Hindering", "Icy", "Illusory", "Incredible", "Inescapable", "Ingenious", "Instant", "Invigorating", "Invisible", "Invulnerable", "Liberating", "Maddening", "Magnificent", "Many-Colored", "Mighty", "Most Excellent", "Omnipotent", "Oozing", "Penultimate", "Pestilential", "Piercing", "Poisonous", "Prismatic", "Raging", "Rejuvenating", "Restorative", "Screaming", "Sensitive", "Shimmering", "Shining", "Silent", "Sleeping", "Slow", "Smoking", "Sorcerer’s", "Strange", "Stupefying", "Terrible", "Thirsty", "Thundering", "Trans-dimensional", "Transmuting", "Ultimate", "Uncontrollable", "Unseen", "Unstoppable", "Untiring", "Vengeful", "Vexing", "Violent", "Violet", "Viridian", "Voracious", "Weakening", "White", "Wondrous", "Yellow"];
 
 function newSpellName() {
-    const roll = Math.floor(Math.random() * 12) + 1;
-    let spellName;
-
-    if (roll <= 2) {
-        spellName = `${pick(property)} ${pick(form)}`;
-    } else if (roll <= 4) {
-        spellName = `${pick(adjective)} ${pick(form)}`;
-    } else if (roll <= 6) {
-        spellName = `${pick(adjective)} ${pick(property)}`;
-    } else if (roll == 7) {
-        spellName = `${pick(form)} of ${pick(property)}`;
-    } else if (roll == 8) {
-        spellName = `${pick(form)} of ${pick(adjective)} ${pick(property)}`;
-    } else if (roll == 9) {
-        spellName = `${wizardName()}'s ${pick(adjective)} ${pick(form)}`;
-    } else if (roll == 10) {
-        spellName = `${wizardName()}'s ${pick(adjective)} ${pick(property)}`;
-    } else if (roll == 11) {
-        spellName = `${wizardName()}'s ${pick(form)} of ${pick(property)}`;
-    } else if (roll == 12) {
-        spellName = `${wizardName()}'s ${pick(property)} ${pick(form)}`;
+    const spellNameMap = {
+        2: `${pick(property)} ${pick(form)}`,
+        4: `${pick(adjective)} ${pick(form)}`,
+        6: `${pick(adjective)} ${pick(property)}`,
+        7: `${pick(form)} of ${pick(property)}`,
+        8: `${pick(form)} of ${pick(adjective)} ${pick(property)}`,
+        9: `${wizardName()}'s ${pick(adjective)} ${pick(form)}`,
+        10: `${wizardName()}'s ${pick(adjective)} ${pick(property)}`,
+        11: `${wizardName()}'s ${pick(form)} of ${pick(property)}`,
+        12: `${wizardName()}'s ${pick(property)} ${pick(form)}`,
     }
-
+    const spellName = spellNameMap[mapKeys(spellNameMap, roll('d12').result)];
+    
     return spellName;
 }
 
